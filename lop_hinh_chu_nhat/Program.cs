7fc2293d4011cb7372a4d059e4c328b47eec4ec4
manhhaktvn <157900250+manhhaktvn@users.noreply.github.com>
2024-03-10 15:13:48 +0700

    Initial commit

---
+++ lop_hinh_chu_nhat/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lop_hinh_chu_nhat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            lop_chu_nhat H1 = new lop_chu_nhat(5, 2);
+            H1.xuat();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lop_hinh_chu_nhat/lop_chu_nha.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lop_hinh_chu_nhat
+{
+    internal class lop_chu_nhat
+    {
+       
+            double width, height;
+
+        public lop_chu_nhat(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double Width { get => width; set => width = value; }
+            public double Height { get => height; set => height = value; }
+        public void xuat()
+            {
+                double dientich = Width * Height;
+                double chuvi = 2 * (Width + Height);
+                Console.Write($"dien tich la: {dientich}, chu vi la: {chuvi}");
+            }
+        }
+}
